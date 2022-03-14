c43f2d6b5ef6b729b9ffa5baba55b0edc20d2edd
cjsrhd94 <cjsrhd94@gmail.com>
2022-03-14 18:42:27 +0900

    first commit

---
+++ src/App.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/반갑습니다./i);
+  expect(linkElement).toBeInTheDocument();
+});
